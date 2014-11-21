fa76cf9746e3ea2e1b531d39eca5be6d8b2d04a7
bjornenalfa <bjornenalfa@users.noreply.github.com>
2014-11-21 13:49:03 +0100

    Update Object.java

--- src/engine/Object.java
+++ src/engine/Object.java
@@ -1,5 +1,12 @@
 package engine;
 
 public class Object {
-
+  Point.Double position;
+  Vector2D massCenter; //relative to position
+  Point.Double velocity;
+  Double rotation;
+  Double angularVelocity;
+  ArrayList<Shape> shapes; //relative to position
+  Double Mass;
+  
 }
