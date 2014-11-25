5a0c8f389d559d217cb4b56172e0544f78530d6e
Letfik <buggi@live.se>
2014-11-25 14:14:51 +0100

    Basic Swing
    JFrame + JPanel som borde gå att rita i, med exempel på hur jag tycker
    vi borde rita

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -1,12 +1,44 @@
 package engine;
 
-public class Main {
-    public String lel(){
-        return "kek noobs v2";
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JFrame {
+    
+    public Main(){
+        setTitle("TITLE");
+        
+        MyJPanel panel = new MyJPanel();
+        panel.setPreferredSize(new Dimension(800,600));
+        setContentPane(panel);
+        
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
     }
-    //YOLO
-    //SWAG
-    public void poop(){
-        return;
+    
+    
+    class MyJPanel extends JPanel {
+        
+        @Override
+        public void update(Graphics g){
+            paintComponent(g);
+        }
+        
+        @Override
+        protected void paintComponent(Graphics g){
+//            for (Shape shape : shapeList) {
+//                shape.paint(g);
+//            }
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        new Main();
     }
 }
