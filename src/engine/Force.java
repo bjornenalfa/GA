5dc08a75df4eafe5179d433ab2a13562dedbb988
Letfik <buggi@live.se>
2014-12-02 13:25:52 +0100

    Force stuff

--- src/engine/Force.java
+++ src/engine/Force.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 
 /**
@@ -7,6 +9,26 @@ import java.awt.Point;
  * @author PastaPojken
  */
 public class Force {
-  Vector2D vector;
+
+    Vector2D vector;
     Point.Double attackPoint;
+
+    public Force(Point.Double attackPoint, Vector2D vector) {
+        this.attackPoint = attackPoint;
+        this.vector = vector;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.RED);
+        double scale = 1;
+        g.drawLine((int) attackPoint.x, (int) attackPoint.y, (int) (vector.getPoint().x * scale), (int) (vector.getPoint().y * scale));
+    }
+    
+    public double getAcc(double m){
+        return vector.getLength()/m;
+    }
+    
+    public double getForce(){
+        return vector.getLength();
+    }
 }
