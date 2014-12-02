2698950f164aa3ec7b292c1965dbfadf3500ae80
bjornenalfa <bjorn.detterfelt@telia.com>
2014-12-02 08:56:58 +0100

    added variables

--- src/engine/World.java
+++ src/engine/World.java
@@ -1,9 +1,13 @@
 package engine;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author PastaPojken
  */
 public class World {
-
+    ArrayList<Object> objects;
+    double time = 0;
+    double g;
 }
