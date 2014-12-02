01ac17f00ea0195523a8ba78cd9f7e8cae51f195
bjornenalfa <bjorn.detterfelt@telia.com>
2014-12-02 13:58:37 +0100

    interpolate fix

--- src/engine/Object.java
+++ src/engine/Object.java
@@ -27,8 +27,9 @@ public class Object {
         nextPosition = move(position, velocity, acceleration, dt);
     }
 
-    public Point.Double interpolate(double k) {
-        return new Point.Double(position.x + (nextPosition.x - position.x) * k, position.y + (nextPosition.y - position.y) * k);
+    public Point.Double interpolate(double k, double dt) {
+        //return new Point.Double(position.x + (nextPosition.x - position.x) * k, position.y + (nextPosition.y - position.y) * k);
+        return new Point.Double(position.x+velocity.x*dt+(acceleration.x*dt*dt)/2, position.y+velocity.y*dt+(acceleration.y*dt*dt)/2);
     }
 
     static public Point.Double move(Point.Double pos, Point.Double vel, Point.Double acc, double dt) {
