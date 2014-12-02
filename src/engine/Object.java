bd2401cdf189f02c1eada377a1f175932f3acf61
bjornenalfa <bjorn.detterfelt@telia.com>
2014-12-02 09:28:51 +0100

    added preliminary movement

--- src/engine/Object.java
+++ src/engine/Object.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  */
 public class Object {
   Point.Double position;
+  Point.Double nextPosition;
   Vector2D massCenter; //relative to position
   Point.Double velocity;
   Double rotation;
@@ -16,4 +17,15 @@ public class Object {
   ArrayList<Shape> shapes; //relative to position
   Double Mass;
   
+  public void update(double dt) {
+      nextPosition = move(position,velocity,dt);
+  }
+  
+  public Point.Double interpolate(double k) {
+      return new Point.Double(position.x+(nextPosition.x-position.x)*k,position.y+(nextPosition.y-position.y)*k);
+  }
+  
+  static public Point.Double move(Point.Double pos, Point.Double vel,double dt) {
+      return new Point.Double(pos.x+vel.x*dt,pos.y+vel.y*dt);
+  }
 }
