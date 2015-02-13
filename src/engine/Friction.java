59f0d1aae632fc42a9aa31fc73dab2ea3156b1d9
Letfik <buggi@live.se>
2015-02-13 15:14:13 +0100

    restitution + more friction

--- src/engine/Friction.java
+++ src/engine/Friction.java
@@ -2,24 +2,23 @@ package engine;
 
 public class Friction {
     
-    
     //Wood = 0, Steel = 1, Rubber = 2, Concrete = 3, Ice = 4, Glass = 5
     
-    static double[][] staticFriction = {
-        {.375,0,0,0,0,0}, 
-        {0,0,0,0,0,0},
-        {0,0,0,1,0,0},
-        {0,0,1,0,0,0},
-        {0,0,0,0,0,0},
-        {0,0,0,0,0,0}
+    private static final double[][] staticFriction = {
+        {.375,.4,1.3,.62,.05,0}, 
+        {.4,.74,1.2,0,0,.6},
+        {1.3,1.2,1.5,1,0.8,1.2},
+        {.62,0,1,0,0,0},
+        {.05,0,.8,0,.1,0},
+        {0,.6,1.2,0,0,.94}
     };
-    static double[][] dynamicFriction = {
-        {.2,0,0,0,0,0}, 
-        {0,0,0,0,0,0},
+    private static final double[][] dynamicFriction = {
+        {.2,.25,0,.42,.03,0}, 
+        {.25,.57,0,0,0,.25},
         {0,0,0,.8,0,0},
-        {0,0,.8,0,0,0},
-        {0,0,0,0,0,0},
-        {0,0,0,0,0,0}
+        {.42,0,.8,0,0,0},
+        {.03,0,0,0,.03,0},
+        {0,.25,0,0,0,.4}
     };
     
     public static double getStatic(int mat1, int mat2){
---
+++ src/engine/Restitution.java
@@ -0,0 +1,14 @@
+package engine;
+
+public class Restitution {
+    
+    //Wood = 0, Steel = 1, Rubber = 2, Concrete = 3, Ice = 4, Glass = 5
+    
+    private static final double[] restitution = {
+        0.4,0.75,0.85,0.2,0.05,0.5
+    };
+    
+    public static double get(int mat){
+        return restitution[mat];
+    }
+}
